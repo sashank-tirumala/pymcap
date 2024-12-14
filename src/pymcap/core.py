91887db39e33b3aeeae2e3155e94209ae3b6da4d
sashank-tirumala <sashanktirumala1@gmail.com>
2024-12-14 11:52:29 -0800

    Added initial pymcap code

--- src/pymcap/core.py
+++ src/pymcap/core.py
@@ -1,16 +1,21 @@
-import pymcap.binaries
 from pathlib import Path
 import subprocess
 
-class PyMCAP():
+
+class PyMCAP:
     def __init__(self):
-        
-        self.executable = (Path(__file__).parent / "binaries/linux_amd64/mcap").resolve()
-        self.__version = subprocess.run([str(self.executable), "version"], stdout=subprocess.PIPE).stdout.decode("utf-8")
-    
+
+        self.executable = (
+            Path(__file__).parent / "binaries/linux_amd64/mcap"
+        ).resolve()
+        self.__version = subprocess.run(
+            [str(self.executable), "version"], stdout=subprocess.PIPE
+        ).stdout.decode("utf-8")
+
     @property
     def version(self):
         return self.__version
+
 
 if __name__ == "__main__":
     p = PyMCAP()
