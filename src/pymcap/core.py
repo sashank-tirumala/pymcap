6fbdec1e0bb09a90027e9e33309b50530ceb9064
sashank-tirumala <sashank@machines.run>
2025-01-24 17:36:01 -0800

    Added some fixes to log level and the way this works

--- src/pymcap/core.py
+++ src/pymcap/core.py
@@ -5,12 +5,15 @@ import sys
 from pathlib import Path
 
 import requests
+import tomli
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class PyMCAP:
-    def __init__(self):
+    def __init__(self, log_level: str = "INFO"):
+        self.logger = logging.getLogger("PyMCAP")
+        self.logger.setLevel(log_level)
         self.logger = logging.getLogger("PyMCAP")
         self.current_dir = Path(__file__).parent
         self.executable = self.get_executable()
@@ -21,9 +24,14 @@ class PyMCAP:
 
 
     @property
-    def version(self):
+    def mcap_cli_version(self):
         return self.__version
     
+    @property
+    def version(self):
+        with open("pyproject.toml", "rb") as f:
+            data = tomli.load(f)
+        return data["project"]["version"]
 
     def __download_executable(self, executable_path: Path, executable_url: str) -> None:
         if executable_path.exists():
@@ -65,7 +73,7 @@ class PyMCAP:
             executable_path = self.current_dir / "binaries/windows_amd64/mcap.bin"
             executable_url = "https://github.com/foxglove/mcap/releases/download/releases%2Fmcap-cli%2Fv0.0.51/mcap-windows-amd64.exe"
         else:
-            raise NotImplementedError(f'Unsupported OS: {os} and architecture: {arch}')
+            raise ValueError(f'Unsupported OS: {os} and architecture: {arch}')
 
         self.logger.debug(f'Executable path: {executable_path}')
         self.logger.debug(f'Executable URL: {executable_url}')
@@ -79,7 +87,7 @@ class PyMCAP:
         elif machine in ('arm64', 'aarch64'):
             return 'arm64'
         else:
-            raise NotImplementedError(f'Unsupported architecture: {machine}')
+            raise ValueError(f'Unsupported architecture: {machine}')
     
     def __get_os(self) -> str:
         system = platform.system().lower()
@@ -90,7 +98,7 @@ class PyMCAP:
         elif system.startswith('windows'):
             return 'windows'
         else:
-            raise NotImplementedError(f'Unsupported OS: {system}')
+            raise ValueError(f'Unsupported OS: {system}')
 
 
 if __name__ == "__main__":
--- tests/test_core.py
+++ tests/test_core.py
@@ -8,6 +8,6 @@ def pymcap() -> PyMCAP:
     return PyMCAP()
 
 
-def test_get_version(pymcap: PyMCAP):
-    assert pymcap.version == 'v0.0.51\n'
+def test_get_mcap_cli_version(pymcap: PyMCAP):
+    assert pymcap.mcap_cli_version == 'v0.0.51\n'
 
